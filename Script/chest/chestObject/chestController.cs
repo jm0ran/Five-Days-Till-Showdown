6bbc32bb62d6a973839704bfa01a91d8cda65b3d
Semi-Otto <ifanfair04@gmail.com>
2021-11-08 10:50:50 -0500

    Update chestController.cs
    Interacts with the drop class, currently adding more stuff, but works and displays the weapon pulled in the console. Can also check to see what file it needs to look inside of for the weapon.

--- Script/chest/chestObject/chestController.cs
+++ Script/chest/chestObject/chestController.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class chestController : MonoBehaviour
 {
+    private drop drop = new drop();
+    private string weaponName;
     // Start is called before the first frame update
     void Start()
     {
-        
+        chest();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    void chest()
+    {
+        if(gameObject.tag == "consumable")
+        {
+            drop.randomValue();
+           weaponName = drop.dropped("consumable.json");
+        }
+
+        if(gameObject.tag == "firearms")
+        {
+            drop.randomValue();
+            weaponName = drop.dropped("firearms.json");
+        }
         
+        if(gameObject.tag == "melee")
+        {
+            drop.randomValue();
+            weaponName = drop.dropped("melee.json");
+        }
+
+        if(gameObject.tag == "goku")
+        {
+            drop.randomValue();
+           weaponName = drop.dropped("goku.json");
+        }
+        Debug.Log(weaponName);
     }
 }
