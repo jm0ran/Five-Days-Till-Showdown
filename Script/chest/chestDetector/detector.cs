6d0c96ef3cc29a499a234da214e1d6ff68702ae3
Semi-Otto <ifanfair04@gmail.com>
2021-11-09 13:48:57 -0500

    Update detector.cs
    commenting

--- Script/chest/chestDetector/detector.cs
+++ Script/chest/chestDetector/detector.cs
@@ -4,36 +4,37 @@ using UnityEngine;
 
 public class detector : MonoBehaviour
 {
+    //set of variables used
     public bool inRange;
     public GameObject player;
     private string weaponName;
-    void Update()
+    void Update()//runs at the start of every frame
     {
-        playerInRange();
+        playerInRange();//call the playerInRange function
     }
-    void playerInRange()
+    void playerInRange()//function that checks when the player is close to the chest
     {
-        if(inRange)
+        if(inRange)//checks to see if the boolean is true
         {
-            if(Input.GetKeyDown(KeyCode.E))
+            if(Input.GetKeyDown(KeyCode.E))//waits for an input press on the E key which means that the player is interacting with the chest
             {
-                player.GetComponent<playerInventory>().insertInventory(gameObject.GetComponent<chestController>().weaponName); 
-                Destroy(gameObject);
+                player.GetComponent<playerInventory>().insertInventory(gameObject.GetComponent<chestController>().weaponName);//gets the player inventory component and sends the weaponName of the item pulled 
+                Destroy(gameObject);//destroys the chest object
             }
         }
     }
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)//checks to see when something has entered the trigger around the chest
     {
-        if(other.gameObject.CompareTag("Player"))
+        if(other.gameObject.CompareTag("Player"))//checks to see if the object in the trigger is the player
         {
-            inRange = true;
+            inRange = true;//sets the inRange boolean to true
         }
     }
-        void OnTriggerExit2D(Collider2D other)
+        void OnTriggerExit2D(Collider2D other)//checks when the object has walked out of the trigger
     {
-        if(other.gameObject.CompareTag("Player"))
+        if(other.gameObject.CompareTag("Player"))//checks to see if the object leaving the trigger is the player
         {
-            inRange = false;
+            inRange = false;//sets the inRange boolean to false
         }
     }
 }
