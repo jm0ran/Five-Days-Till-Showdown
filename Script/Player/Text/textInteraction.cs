50de4bf09a8ddf1aa46093e9f44f1fa840fd0204
Semi-Otto <ifanfair04@gmail.com>
2021-11-09 13:49:36 -0500

    Update textInteraction.cs
    commenting

--- Script/Player/Text/textInteraction.cs
+++ Script/Player/Text/textInteraction.cs
@@ -4,20 +4,21 @@ using UnityEngine;
 
 public class textInteraction : MonoBehaviour
 {
+    //set of variables
     public bool noInput = false;
     public Canvas canvas;
     // Update is called once per frame
     void Update()
     {
-        Canvas render = canvas.GetComponent<Canvas>();
-        if (render.enabled == true)
+        Canvas render = canvas.GetComponent<Canvas>();//gets the canvas component off the canvas
+        if (render.enabled == true)//checks to see if the canvas gameObject is being rendered
         {
-            noInput = true;
+            noInput = true;//sets noInput to true stopping the player from moving
         }
-        if (render.enabled == true && Input.GetKeyDown(KeyCode.E))
+        if (render.enabled == true && Input.GetKeyDown(KeyCode.E))//checks to see if the canvas is enabled and the E key has been pressed
         {
-            noInput = false;
-            render.enabled = false;
+            noInput = false;//sets noInput to false allowing the player to move
+            render.enabled = false;//turns the renderer off now that the player can move
         }
     }
 }
