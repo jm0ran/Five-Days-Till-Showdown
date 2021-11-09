74dbaf49abcf0dc702ba3bc1514be66718bfde1f
Semi-Otto <ifanfair04@gmail.com>
2021-11-09 13:49:48 -0500

    Update checkpoint.cs
    commenting

--- Script/world/tutorial/dialogueSystem/checkpoint.cs
+++ Script/world/tutorial/dialogueSystem/checkpoint.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 public class checkpoint : MonoBehaviour
 {
+    //set of variables used
     public Transform player;
     public Canvas canvas;
     public Text dialogue;
@@ -11,22 +12,22 @@ public class checkpoint : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Canvas render = canvas.GetComponent<Canvas>();
-        render.enabled = false;
+        Canvas render = canvas.GetComponent<Canvas>();//gets the canvas component off the canvas gameObject
+        render.enabled = false;//sets the renderer to false
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (player.position == transform.position && !Input.GetKeyDown(KeyCode.E))
+        if(player.position == transform.position && !Input.GetKeyDown(KeyCode.E))//checks if the player is standing on the same tile as the checkpoint flag
         {
-            Canvas render = canvas.GetComponent<Canvas>();
-            render.enabled = true;
-            dialogue.text = dialogueOutput;
+            Canvas render = canvas.GetComponent<Canvas>();//gets the canvas component off the canvas
+            render.enabled = true;//sets the renderer to true
+            dialogue.text = dialogueOutput;//outs the dialogue for the text. The dialogue is written publicly for easy changes
         }
-        if (player.position == transform.position && Input.GetKeyDown(KeyCode.E))
+        if(player.position == transform.position && Input.GetKeyDown(KeyCode.E))//checks if the player is still on the flag and has pressed E
         {
-            gameObject.SetActive(false);
+            gameObject.SetActive(false);//turns the flag gameObject off
         }
     }
 }
