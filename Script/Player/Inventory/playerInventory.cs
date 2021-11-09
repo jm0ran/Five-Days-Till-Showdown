a23c0ccfb9a9cf8076e37b8de2fdd221e4e2a2b9
Semi-Otto <ifanfair04@gmail.com>
2021-11-09 10:43:48 -0500

    Update playerInventory.cs
    rewrote currently works for intended purpose. Will add more feature later on

--- Script/Player/Inventory/playerInventory.cs
+++ Script/Player/Inventory/playerInventory.cs
@@ -1,21 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-namespace Player
+
+public class playerInventory : MonoBehaviour
 {
-    public class playerInventory : MonoBehaviour
+    public List<string> inventory = new List<string>();//future use for viewing items obtained
+
+    public void insertInventory(string itemName)
     {
-        List<string> inventory = new List<string>();//future use for viewing items obtained
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
-
-        public void looting(string newWeapon)//make it so that you can't move when action is true and set a small time to open the chest
-        {
-            inventory.Add(newWeapon);//adds weapon from chest to player inventory
-        }
+        inventory.Add(itemName);
+        Debug.Log(itemName);
     }
 }
+
