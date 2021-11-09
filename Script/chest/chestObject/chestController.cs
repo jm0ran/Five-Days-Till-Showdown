ea71d97b615165e9f553ae0f28ef1e4a85da6c08
Semi-Otto <ifanfair04@gmail.com>
2021-11-09 13:49:04 -0500

    Update chestController.cs
    commenting

--- Script/chest/chestObject/chestController.cs
+++ Script/chest/chestObject/chestController.cs
@@ -4,36 +4,36 @@ using UnityEngine;
 
 public class chestController : MonoBehaviour
 {
-    private drop drop = new drop();
-    public string weaponName;
-    void Start() 
+    private drop drop = new drop();//instance of the drop object
+    public string weaponName;//weaponName variable to store the name of the item pulled
+    void Start()//Runs at the start of the game 
     {
-        chest();
+        chest();//calls the chest function
     }
-    void chest()
+    void chest()//obtains the item that the chest will give to the player when opened
     {
-        if(gameObject.tag == "consumable")
+        if(gameObject.tag == "consumable")//checks the tag of the chest
         {
-            drop.randomValue();
-           weaponName = drop.dropped("consumable.json",4);
+            drop.randomValue();//creates a random value used for the dropped function
+           weaponName = drop.dropped("consumable.json",4);//runs the dropped function giving the file name and list size
         }
 
-        if(gameObject.tag == "firearms")
+        if(gameObject.tag == "firearms")//checks the tag of the chest
         {
-            drop.randomValue();
-            weaponName = drop.dropped("firearms.json",4);
+            drop.randomValue();//creates a random value used for the dropped function
+            weaponName = drop.dropped("firearms.json",4);//runs the dropped function giving the file name and list size
         }
         
-        if(gameObject.tag == "melee")
+        if(gameObject.tag == "melee")//checks the tag of the chest
         {
-            drop.randomValue();
-            weaponName = drop.dropped("melee.json",6);
+            drop.randomValue();//creates a random value used for the dropped function
+            weaponName = drop.dropped("melee.json",6);//runs the dropped function giving the file name and list size
         }
 
-        if(gameObject.tag == "goku")
+        if(gameObject.tag == "goku")//checks the tag of the chest
         {
-           drop.randomValue();
-           weaponName = drop.dropped("goku.json",7);
+           drop.randomValue();//creates a random value used for the dropped function
+           weaponName = drop.dropped("goku.json",7);//runs the dropped function giving the file name and list size
         }
     }
 }
