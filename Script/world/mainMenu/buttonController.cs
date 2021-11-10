a98744981ab68f472a2f91cd5e0ac7605e56b8c0
Semi-Otto <ifanfair04@gmail.com>
2021-11-10 13:35:29 -0500

    Update buttonController.cs
    just added some comments. Will add more to the script in future.

--- Script/world/mainMenu/buttonController.cs
+++ Script/world/mainMenu/buttonController.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+/*Script used to manage all the buttons on the main menu
+All the functions are the same names as the buttons
+Currently only the tutorial button has an event that takes the player to the tutorial level that is currently being developed
+*/
 public class buttonController : MonoBehaviour
 {
-    public void tutorial()
+    public void tutorial()//tutorial function attached to a button
     {
-        SceneManager.LoadScene("tutorial");
+        SceneManager.LoadScene("tutorial");//loads the tutorial scene
     }
-    public void play()
+    public void play()//play function that is attached to button no actions yet
     {
 
     }
-    public void settings()
+    public void settings()//settings function that is attached to button no actions yet
     {
 
     }
