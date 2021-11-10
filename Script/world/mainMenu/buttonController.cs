05a6b4cbb6ec4fa529dd462c0aad98aee6eda73d
Semi-Otto <ifanfair04@gmail.com>
2021-11-10 10:21:25 -0500

    Create buttonController.cs
    button controller for the main menu. Will add more functionality once I need the other buttons to do something.

---
+++ Script/world/mainMenu/buttonController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class buttonController : MonoBehaviour
+{
+    public void tutorial()
+    {
+        SceneManager.LoadScene("tutorial");
+    }
+    public void play()
+    {
+
+    }
+    public void settings()
+    {
+
+    }
+}
