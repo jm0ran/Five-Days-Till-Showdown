4648e13a9f0eb4f4d49be39ce94585f32726669d
Semi-Otto <ifanfair04@gmail.com>
2021-11-10 13:37:27 -0500

    Update combatTrigger.cs
    Added some comments

--- Script/zombie/combatTrigger/combatTrigger.cs
+++ Script/zombie/combatTrigger/combatTrigger.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+/*Current combat trigger system
+When the player acts the trigger event the combat scene is loaded
+This is the final part of the tutorial that needs to be built then the main game can be developed
+*/
 public class combatTrigger : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)//checks for the triggerEnter event
     {
-        if(other.gameObject.CompareTag("Player"))
+        if(other.gameObject.CompareTag("Player"))//checks to see if the object is the Player
         {
-            SceneManager.LoadScene("Combat");
+            SceneManager.LoadScene("Combat");//loads the combat scene
         }
     }
 }
